71f0e99c44b46b14c2b300747faa54d30d74ff7f
NemishViradiya <154655825+NemishViradiya@users.noreply.github.com>
2023-12-29 18:00:19 +0530

    Add files via upload

---
+++ n_5.dart
@@ -0,0 +1,17 @@
+import 'dart;io';
+
+void mani()
+{
+  print("Enter any namber");
+  int num =int.parse(stdin.readLineSync().tostring());
+  if(num>0)
+{
+  print("number is  positive");
+  {
+  else if (num == 0) {
+    print("Number is neutral or zero");
+  }
+  else
+  {
+    print("number is negative");
+  }
