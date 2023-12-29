9809b1025bb31bc363d877da21c031de7aecd090
NemishViradiya <154655825+NemishViradiya@users.noreply.github.com>
2023-12-29 15:57:03 +0530

    Add files via upload

---
+++ untitled1.dart
@@ -0,0 +1,63 @@
+void main() {
+  print("neme:nemish viradiya /n");
+  print("birth date : 8/10/2005 /n");
+  print("age :18/n");
+  print(
+      "address : madhav park sheri no 4, oppo.real prime ,bapa sitaram chok
+
+  print(2 + 2);
+  print(6 - 3);
+  print(5 * 3);
+  print(10 / 2);
+
+  var someInt = 3;
+  print("square of 3  ${someInt * someInt}");
+
+  print("cube of 3    ${someInt * someInt * someInt}")
+
+  const double pI =3.15;
+  var r = 13;
+  print("Aeea of circle  ${pI * r * r}");
+
+
+  const double someVal = 0.5;
+  double base =32.1;
+  double height = 14;
+
+  print("area of traingle = ${someVal * base * height}");
+
+
+  print("simple Interest  ;- ${simpleInt(10000 100 10)}");
+
+  double mark0fsubjuct1 =83.34;
+  double mark0fsubjuct2 =87.8 ;
+  double mark0fsubjuct3 =76.9;
+  double mark0fsubjuct4 =89.3 ;
+  double mark0fsubjuct5 =69.3;
+
+  double sum = mark0fsubjuct4 +
+    mark0fsubjuct5 +
+    mark0fsubjuct3 +
+    mark0fsubjuct2 +
+    mark0fsubjuct1;
+
+   print("sum of 5 subject  ;- $sum");
+   print("percentage  :- ${sum /  5}");
+
+}
+   double simpleInt(double p, double r, double n ) {
+
+  return ( p * r * n)  / 100;
+}
+
+
+
+
+
+
+
+
+
+
+
+
