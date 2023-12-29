7da46a9685f6bc09f2db448a43ea22cd5cd57257
NemishViradiya <154655825+NemishViradiya@users.noreply.github.com>
2023-12-29 17:27:31 +0530

    Add files via upload

---
+++ n_2.dart
@@ -0,0 +1,27 @@
+
+void main()
+
+{
+  int marks =60:
+
+  if ( mark>=70)
+{
+  print("A Grade");
+}
+  else if (marks>=60)
+  {
+    print("B Grade");
+  }
+  else if (marks>=50)
+    {
+      print( "c Grade");
+    }
+  else if (marks>=40)
+    {
+      print("D Gtrade");
+
+    }
+  else
+    {
+      print("fail");
+    }
