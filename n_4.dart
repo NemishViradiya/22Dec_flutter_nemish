083b3864a2e339b91ffade64a572b818428eeddc
NemishViradiya <154655825+NemishViradiya@users.noreply.github.com>
2023-12-29 17:46:21 +0530

    Add files via upload

---
+++ n_4.dart
@@ -0,0 +1,17 @@
+void main()
+{
+  int age =68;
+   if (age>18) {
+     print("ELigible to vote");
+     if (age > 60) {
+       print("senior citizen");
+     }
+     else {
+       print("young age");
+     }
+   }
+   else
+     {
+       print("not Eligible to vote")
+
+     }
