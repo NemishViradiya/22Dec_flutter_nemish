193b02291af07fec2eba7fd1156b9aa572c61a10
NemishViradiya <154655825+NemishViradiya@users.noreply.github.com>
2023-12-30 15:46:12 +0530

    Add files via upload

---
+++ subjects_hw.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+    void main(){
+      print("your maths is");
+  int maths = int.parse(stdin.readLineSync().toString());
+
+  print("your maths is");
+  int science =int.parse(stdin.readLineSync().toString());
+
+  print("your maths is");
+  int hindi =int.parse(stdin.readLineSync().toString());
+
+  print ("your maths is");
+  int english =int.parse(stdin.readLineSync().toString());
+
+  print ("your maths is");
+  int sanskrit =int.parse(stdin.readLineSync().toString());
+
+  print ("your maths is");
+  int gujarati =int.parse(stdin.readLineSync().toString());
+
+  int sum = gujarati+science+english+hindi+sanskrit;
+      print (sum);
+  double percentage =(sum)/5;
+      print (percentage);
+
+
+    }
