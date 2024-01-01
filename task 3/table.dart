88124381d395c2260eb780ff8821c9c8cfc66d2f
NemishViradiya <154655825+NemishViradiya@users.noreply.github.com>
2024-01-01 16:22:20 +0530

    Add files via upload

---
+++ task 3/table.dart
@@ -0,0 +1,10 @@
+import 'dart :io';
+void mani()
+{
+  print ("Enter Any Number For the Table");
+  int num = int.parse(stdin.redLinsSync().toString());
+  var no = num;
+  for (var i=1; i<=10; i++)
+{
+  print ($num *$i =${num*i}");
+}
