e63d189f71b1627f36f12796c038e3890285516c
NemishViradiya <154655825+NemishViradiya@users.noreply.github.com>
2024-01-01 16:11:18 +0530

    Add files via upload

---
+++ task 3/monday to sunday task edart.dart
@@ -0,0 +1,29 @@
+import'dart:io'
+    void main()
+    {
+      print("Enter Any Number");
+      int num = int.parse(studin(stdin.readLineSync().toString());
+      switch(num)
+      {
+        case 1 : print(" your selectered Day is monday");
+        //break
+
+    case 2 : print(" your selectered Day is Tuesday");
+    //break
+
+    case 3 : print(" your selectered Day is wednesday");
+    //break
+
+    case 4 : print(" your selectered Day is Thursday");
+    //break
+
+    case 5 : print(" your selectered Day is friday");
+    //break
+
+    case 6 : print(" your selectered Day is saturday");
+    //break
+
+    case 7 : print(" your selectered Day is sunday");
+    //break
+      }
+    }
